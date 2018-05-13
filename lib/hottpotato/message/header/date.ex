c19c76258002479739c402146ab32e6f421ae8b0
Michael Pope <mpope.cr@gmail.com>
2018-05-13 00:56:50 -0700

    Add message header date

---
+++ lib/hottpotato/message/header/date.ex
@@ -0,0 +1,146 @@
+alias HoTTPotato.Message.HeaderEncoder
+alias HoTTPotato.Message.Header.Date, as: HeaderDate
+
+defmodule HeaderDate do
+  defstruct [
+    :name,
+    :value
+  ]
+
+  @type datetime :: DateTime.t()
+  @type name :: String.t()
+  @type t :: %__MODULE__{
+    name: name(),
+    value: datetime()
+  }
+
+  #######
+  # API #
+  #######
+
+  @spec encode(t()) :: [String.t(), ...]
+  def encode(%__MODULE__{} = hd) do
+    HeaderEncoder.encode(hd)
+  end
+
+  @spec new(DateTime.t()) :: t()
+  def new(%DateTime{} = dt \\ DateTime.utc_now()) do
+    __MODULE__
+    |> struct!(name: "Date")
+    |> struct!(value: dt)
+  end
+
+  ##########################
+  # Private / Undocumented #
+  ##########################
+
+  @doc false
+  @spec day_of_month(Integer.t()) :: String.t()
+  def day_of_month(int)
+
+  def day_of_month(int) when int < 10, do: "0#{int}"
+
+  def day_of_month(int), do: "#{int}"
+
+  @doc false
+  @spec day_of_week(Integer.t(), Integer.t(), Integer.t()) :: String.t()
+  def day_of_week(year, month, day) do
+    case Calendar.ISO.day_of_week(year, month, day) do
+      1 -> "Mon"
+      2 -> "Tue"
+      3 -> "Wed"
+      4 -> "Thu"
+      5 -> "Fri"
+      6 -> "Sat"
+      7 -> "Sun"
+    end
+  end
+
+  @doc false
+  @spec hour(Integer.t()) :: String.t()
+  def hour(int)
+
+  def hour(int) when int < 10, do: "0#{int}"
+
+  def hour(int), do: "#{int}"
+
+  @doc false
+  @spec minute(Integer.t()) :: String.t()
+  def minute(int)
+
+  def minute(int) when int < 10, do: "0#{int}"
+
+  def minute(int), do: "#{int}"
+
+  @doc false
+  @spec month(Integer.t()) :: String.t()
+  def month(int) do
+    case int do
+      1 -> "Jan"
+      2 -> "Feb"
+      3 -> "Mar"
+      4 -> "Apr"
+      5 -> "May"
+      6 -> "Jun"
+      7 -> "Jul"
+      8 -> "Aug"
+      9 -> "Sep"
+      10 -> "Oct"
+      11 -> "Nov"
+      12 -> "Dec"
+    end
+  end
+
+  @doc false
+  @spec second(Integer.t()) :: String.t()
+  def second(int)
+
+  def second(int) when int < 10, do: "0#{int}"
+
+  def second(int), do: "#{int}"
+
+  @doc false
+  @spec year(Integer.t()) :: String.t()
+  def year(int)
+
+  def year(int) when int < 10, do: "000#{int}"
+
+  def year(int) when int < 100, do: "00#{int}"
+
+  def year(int) when int < 1000, do: "0#{int}"
+
+  def year(int), do: "#{int}"
+end
+
+defimpl HeaderEncoder, for: HeaderDate do
+  #######
+  # API #
+  #######
+
+  @doc """
+  Based on format in [RFC 7766](https://tools.ietf.org/html/rfc2616#page-20)
+  """
+  @spec encode(HeaderDate.t()) :: [String.t(), ...]
+  def encode(%HeaderDate{name: name, value: %DateTime{} = dt}) do
+    [
+      name,
+      ": ",
+      HeaderDate.day_of_week(dt.year, dt.month, dt.day),
+      ", ",
+      HeaderDate.day_of_month(dt.day),
+      " ",
+      HeaderDate.month(dt.month),
+      " ",
+      HeaderDate.year(dt.year),
+      " ",
+      HeaderDate.hour(dt.hour),
+      ":",
+      HeaderDate.minute(dt.minute),
+      ":",
+      HeaderDate.second(dt.second),
+      " ",
+      dt.zone_abbr,
+      "\r\n"
+    ]
+  end
+end
---
+++ test/hottpotato/message/header/date_test.exs
@@ -0,0 +1,87 @@
+defmodule HoTTPotato.Message.Header.DateTest do
+  use ExUnit.Case, async: true
+
+  alias HoTTPotato.Message.Header.Date, as: HeaderDate
+
+  #######
+  # API #
+  #######
+
+  test "&encode/1" do
+    dt =
+      DateTime.utc_now()
+      |> struct(day: 1)
+      |> struct(month: 5)
+      |> struct!(year: 2018)
+      |> struct!(hour: 2)
+      |> struct!(minute: 3)
+      |> struct!(second: 4)
+    actual =
+      dt
+      |> HeaderDate.new()
+      |> HeaderDate.encode()
+      |> IO.iodata_to_binary()
+    expected = "Date: Tue, 01 May 2018 02:03:04 UTC\r\n"
+    assert actual == expected
+  end
+
+  test "&new/1" do
+    assert match?(%HeaderDate{}, HeaderDate.new())
+  end
+
+  ##########################
+  # Private / Undocumented #
+  ##########################
+
+  test "&day_of_month/1" do
+    assert HeaderDate.day_of_month(1) == "01"
+    assert HeaderDate.day_of_month(10) == "10"
+  end
+
+  test "&day_of_week/1" do
+    assert HeaderDate.day_of_week(2018, 5, 1) == "Tue"
+    assert HeaderDate.day_of_week(2018, 5, 2) == "Wed"
+    assert HeaderDate.day_of_week(2018, 5, 3) == "Thu"
+    assert HeaderDate.day_of_week(2018, 5, 4) == "Fri"
+    assert HeaderDate.day_of_week(2018, 5, 5) == "Sat"
+    assert HeaderDate.day_of_week(2018, 5, 6) == "Sun"
+    assert HeaderDate.day_of_week(2018, 5, 7) == "Mon"
+  end
+
+  test "&hour/1" do
+    assert HeaderDate.hour(1) == "01"
+    assert HeaderDate.hour(10) == "10"
+  end
+
+  test "&minute/1" do
+    assert HeaderDate.minute(1) == "01"
+    assert HeaderDate.minute(10) == "10"
+  end
+
+  test "&month/1" do
+    assert HeaderDate.month(1) == "Jan"
+    assert HeaderDate.month(2) == "Feb"
+    assert HeaderDate.month(3) == "Mar"
+    assert HeaderDate.month(4) == "Apr"
+    assert HeaderDate.month(5) == "May"
+    assert HeaderDate.month(6) == "Jun"
+    assert HeaderDate.month(7) == "Jul"
+    assert HeaderDate.month(8) == "Aug"
+    assert HeaderDate.month(9) == "Sep"
+    assert HeaderDate.month(10) == "Oct"
+    assert HeaderDate.month(11) == "Nov"
+    assert HeaderDate.month(12) == "Dec"
+  end
+
+  test "&second/1" do
+    assert HeaderDate.second(1) == "01"
+    assert HeaderDate.second(10) == "10"
+  end
+
+  test "&year/1" do
+    assert HeaderDate.year(1) == "0001"
+    assert HeaderDate.year(10) == "0010"
+    assert HeaderDate.year(100) == "0100"
+    assert HeaderDate.year(1000) == "1000"
+  end
+end
